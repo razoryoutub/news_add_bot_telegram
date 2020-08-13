5d55a52e54c31c282fe6201acb45691bf1b74489
Oleg <razoryoutub@mail.ru>
2020-08-14 01:31:59 +0300

    start bot

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from aiogram import Bot
+from aiogram import Bot, Dispatcher, types, executor
 import asyncio
 import aiohttp
 token = '1225831912:AAG60sUGFoeRVePSufgLFM0Xaat176T2t1o'
 bot = Bot(token)
 dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def handle_start_help(message: types.Message):
+	await message.answer('Привет, этот бот умеет то то и то')
+
+
+@dp.message_handler( content_types=['text'])
+async def by_massage(message: types.Message):
+	await message.answer(message.text)
+
+
+if __name__ == '__main__':	
+	executor.start_polling(dp, skip_updates=True)
